298ef2251a9221451260efdc9a9c8f09d9bd219e
Luna Sarmiento <selene942008@hotmail.com>
2021-09-06 18:07:33 +0200

    pdf

--- src/utils/pdf.js
+++ src/utils/pdf.js
@@ -1,36 +1,42 @@
 import PdfPrinter from "pdfmake";
-import { pipeline } from "stream";
 import fs from "fs-extra";
-
-const { createWriteStream } = fs 
+import { Buffer } from "buffer";
 
 const fonts = {
   Roboto: {
     normal: "Helvetica",
     bold: "Helvetica-Bold",
     italics: "Helvetica-Oblique",
-    bolditalics: "Helvetica-BoldOblique"
-  }
+    bolditalics: "Helvetica-BoldOblique",
+  },
+};
+
+function base64_encode(file) {
+  var bitmap = fs.readFileSync(file);
+  return new Buffer(bitmap).toString("base64");
 }
 
-const printer = new PdfPrinter(fonts)
+const printer = new PdfPrinter(fonts);
 
 export const getPDFReadableStream = (user) => {
   const docDefinition = {
     content: [
-      {text: user.name,style: 'header'}
-    //   {text: `\nby ${blog.author.name}`,style: 'subheader'},     
-    // //   `\n${striptags(blog.content)}`,
-    //   {text: `\n\nCREATED AT ${new Date(blog.createdAt)}`,style: 'small'}
+      /* { image: `${base64_encode(user.image)}` }, */
+      { text: user.name + " " + user.surname, style: "header" },
+      { text: user.email, style: "subheader" },
+      { text: user.bio, style: "small" },
     ],
     styles: {
-      header: {fontSize: 18,bold: true}
-    //   subheader: {fontSize: 15,bold: true},
-    //   small: {fontSize: 8}
-    }
-  }
-  const options = {}
-  const pdfReadableStream = printer.createPdfKitDocument(docDefinition, options)
-  pdfReadableStream.end()
-  return pdfReadableStream 
-}
+      header: { fontSize: 18, bold: true },
+      subheader: { fontSize: 15, bold: true },
+      small: { fontSize: 8 },
+    },
+  };
+  const options = {};
+  const pdfReadableStream = printer.createPdfKitDocument(
+    docDefinition,
+    options
+  );
+  pdfReadableStream.end();
+  return pdfReadableStream;
+};
